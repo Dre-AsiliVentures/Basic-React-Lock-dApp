a14c319356eb8dbc07b369045e06dcb447bd4f7e
Dre <61284769+Dre-AsiliVentures@users.noreply.github.com>
2025-03-10 16:31:57 +0300

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Lock from "./Lock";
+
+function App() {
+  return (
+    <div style={{ background: "#f4f4f4", minHeight: "100vh", padding: "50px" }}>
+      <Lock />
+    </div>
+  );
+}
+
+export default App;
